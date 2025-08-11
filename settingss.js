c5c4c72cbbbf166f4ef6e8a29c38dbb07dae46fb
mensahsamueljunior940-dot <mensahsamueljunior940@gmail.com>
2025-08-11 13:17:21 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~VnMiDAYY#ifbhCA5D9MBN09_kqBr_ddN-hT7sFGoNs96VUJHPiY4",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "233534647420", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
